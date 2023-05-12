f789f3721f7c5fa5f7d56422347df250ce210b08
David Del Sol <daviddelsol1998@gmail.com>
2023-05-12 13:32:12 -0500

    fix: switch units to rem for mobile responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import "./App.css";
-
+import { Container } from "react-bootstrap";
 import Navigation from "./components/navigation/navigation";
 
 import Footer from "./components/footer/footer";
 
 function App() {
   return (
-    <div className="container">
-      <Navigation />
+    <div className="d-flex flex-column min-vh-100">
+      <Container fluid="lg" className="App">
+        <Navigation />
 
-      <Footer />
+        <Footer />
+      </Container>
     </div>
   );
 }
