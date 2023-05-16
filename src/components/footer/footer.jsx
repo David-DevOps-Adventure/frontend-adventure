4b7774b8069f9deb4a689d4daf9d1df047d134d7
David Del Sol <daviddelsol1998@gmail.com>
2023-05-16 16:42:20 -0500

    fix: Added temporary padding to the footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,7 @@ let currentYear = new Date().getFullYear();
 
 const Footer = () => (
   <div className="footer">
-    <footer className="py-2 fixed-bottom dark-section">
+    <footer className="py-2  dark-section">
       <div className="container text-center">
         <small>DevOps with David {currentYear} &copy; David Del Sol</small>
       </div>
